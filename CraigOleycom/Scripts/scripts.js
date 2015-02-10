a37f3cc430cbeae0cb0ad1b583ecfc67811416e5
Craig Oley <CraigOley@gmail.com>
2015-02-09 23:43:29 -0500

    Some style changes

--- CraigOleycom/Scripts/scripts.js
+++ CraigOleycom/Scripts/scripts.js
@@ -1,16 +1,17 @@
-﻿$(document).ready(function () {
+﻿$(document).ready(function() {
     bgImgChange();
     bgImgSlideshow();
+    $('[data-toggle="tooltip"]').tooltip();
 });
 
-var bgImgSlideshow = function() {
+var bgImgSlideshow = function () {
     bgImgChange();
 
     setTimeout(function () {
         bgImgSlideshow();
     }, 8000);
 
-}
+};
 
 function bgImgChange() {
     $(".bgImg").css({
@@ -19,9 +20,16 @@ function bgImgChange() {
         '-moz-transition': 'opaicty 0s'
     });
     var images = Array(
-        "https://ppcdn.500px.org/98393139/676608e61b22af89cd6a51fd85e96614f6e5c0a8/2048.jpg",
-        "https://ppcdn.500px.org/98393137/e2aa24b6017f2810ae22d566f40c493d1f615e63/2048.jpg",
-        "https://ppcdn.500px.org/98393135/ae2f113c7b58c5aa31c4c42762f9a8e62986552b/2048.jpg");
+        "https://drscdn.500px.org/photo/98434343/m=900/cebcaf988838055092036df59043cbfc",
+        "https://drscdn.500px.org/photo/98393137/m=900/de2c2e0e7511b869d512b32dda1e5495",
+        "https://drscdn.500px.org/photo/98393125/m=900/5535c06e50e7b045b0d0269e2c743013",
+        "https://drscdn.500px.org/photo/98393135/m=900/b507261fe70abf3b997eead2bc163080",
+        "https://drscdn.500px.org/photo/98393133/m=900/f606d6337e238cc82ebb7a651300a41f",
+        "https://drscdn.500px.org/photo/98393139/m=900/acfbf97c0d8c63eb2ea72180da1f1572",
+        "https://drscdn.500px.org/photo/98434347/m=900/5f92ade8da7652d1e2a2d1cf6c2b4a80",
+        "https://drscdn.500px.org/photo/98434335/m=900/451309b22d792305f4585446c12fa050",
+        "https://drscdn.500px.org/photo/98434329/m=900/6e0a6a243e6fee0ddcc279181d1d3b19",
+        "https://drscdn.500px.org/photo/98434319/m=900/e9d4ff3c1afc3763e97959af92c91ac5");
     var last_img = $("#bgImg").css('background-image');
     if (last_img === "none") {
         last_img = images[0];
