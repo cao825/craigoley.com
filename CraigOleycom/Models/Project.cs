25941c40bef63f056aae08e09682a838d4366da2
Craig Oley <CraigOley@gmail.com>
2015-02-28 11:10:03 -0500

    Fixed resume display issue

--- CraigOleycom/Models/Project.cs
+++ CraigOleycom/Models/Project.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CraigOleycom.Models
 {
@@ -9,10 +11,14 @@ namespace CraigOleycom.Models
     {
         public int ID { get; set; }
         public string name { get; set; }
+        [Display(Name = "Short Description")]
         public string short_description { get; set; }
+        [Display(Name = "Description")]
         public string description { get; set; }
         public string img_url { get; set; }
+        [Display(Name = "Repository URL")]
         public string repo_url { get; set; }
+        [Display(Name = "Link")]
         public string target_url { get; set; }
         public IEnumerable<Technology> technologies { get; set; }
     }
